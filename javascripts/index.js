169e18dd9daa9f2881ba44de3a8208659030676e
Jonah Unuafe <fejisfejiro@gmail.com>
2023-12-13 12:37:37 +0100

    added new features

---
+++ javascripts/index.js
@@ -0,0 +1,27 @@
+const menu = document.querySelector("#menu");
+const close = document.querySelector("#close");
+const nav = document.querySelector(".nav");
+
+
+
+function changeMenuToTimes() {
+    if(menu.getAttribute("src") === "/starter-code/assets/shared/mobile/icon-hamburger.svg") {
+       menu.setAttribute("src", "/starter-code/assets/shared/mobile/icon-close.svg");
+    } else {
+        menu.setAttribute("src", "/starter-code/assets/shared/mobile/icon-hamburger.svg")
+    }
+}
+
+function openNav(event) {
+    nav.classList.toggle("show");
+    event.stopPropagation();
+} 
+
+menu.addEventListener("click", openNav);
+
+document.addEventListener("click", function(event) {
+    if (!nav.contains(event.target)) {
+      nav.classList.remove("show");
+      menu.setAttribute("src", "/starter-code/assets/shared/mobile/icon-hamburger.svg");
+    }
+});
