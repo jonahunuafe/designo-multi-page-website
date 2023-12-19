cb272e2bbdc8380b3915be9c4d81323379e214eb
Jonah Unuafe <fejisfejiro@gmail.com>
2023-12-19 11:49:33 +0100

    added new features

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,38 +1,26 @@
 const menu = document.getElementById("menu");
 const nav = document.querySelector(".nav");
-const test = false;
 
 function changeMenuToTimes() {
-  if (menu.getAttribute('src') === "../assets/shared/mobile/icon-hamburger.svg") {
-        menu.setAttribute('src', "../assets/shared/mobile/icon-close.svg");
+    if (menu.getAttribute('src') === '/starter-code/assets/shared/mobile/icon-hamburger.svg') {
+        menu.setAttribute('src', '/starter-code/assets/shared/mobile/icon-close.svg');
     }
-    else {
-        setAttribute('src', "../assets/shared/mobile/icon-hamburger.svg");
+      else {
+        menu.setAttribute('src', '/starter-code/assets/shared/mobile/icon-hamburger.svg');
     }  
 }
 
 function openNav(event) {
-    nav.classList.toggle("show");
-    menu.setAttribute('src', "../assets/shared/mobile/icon-close.svg");
+    nav.classList.toggle('show');
     event.stopPropagation();
 } 
 
-menu.addEventListener("click", changeMenuToTimes)
-menu.addEventListener("click", openNav);
-//close.addEventListener("click", changeTimesToMenu);
+menu.addEventListener('click', changeMenuToTimes)
+menu.addEventListener('click', openNav);
 
-document.addEventListener("click", function(event) {
+document.addEventListener('click', function(event) {
     if (!nav.contains(event.target)) {
-      nav.classList.remove("show");
-      menu.setAttribute("src", "../assets/shared/mobile/icon-hamburger.svg");
+      nav.classList.remove('show');
+      menu.setAttribute("src", "/starter-code/assets/shared/mobile/icon-hamburger.svg");
     }
 });
-
-
-
-// if (menu.getAttribute('src') === "/starter-code/assets/shared/mobile/icon-hamburger.svg") {
-//     menu.setAttribute('src', "/starter-code/assets/shared/mobile/icon-close.svg");
-// }
-//   else {
-//     menu.setAttribute('src', "/starter-code/assets/shared/mobile/icon-hamburger.svg");
-// }
