be07e1c0e105a1230386e1a11374af5e39599a1c
Jonah Unuafe <fejisfejiro@gmail.com>
2023-12-19 10:30:26 +0100

    added new features

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,22 +1,25 @@
 const menu = document.querySelector("#menu");
-const close = document.querySelector("#close");
 const nav = document.querySelector(".nav");
-
+const test = false;
 
 function changeMenuToTimes() {
-    if(menu.getAttribute("src") === "/starter-code/assets/shared/mobile/icon-hamburger.svg") {
-       menu.setAttribute("src", "/starter-code/assets/shared/mobile/icon-close.svg");
-    } else {
-        menu.setAttribute("src", "/starter-code/assets/shared/mobile/icon-hamburger.svg")
+  if (menu.getAttribute('src') === "/starter-code/assets/shared/mobile/icon-hamburger.svg") {
+        menu.setAttribute('src', "/starter-code/assets/shared/mobile/icon-close.svg");
     }
+    else {
+        setAttribute('src', "/starter-code/assets/shared/mobile/icon-hamburger.svg");
+    }  
 }
 
 function openNav(event) {
     nav.classList.toggle("show");
+    menu.setAttribute('src', "/starter-code/assets/shared/mobile/icon-close.svg");
     event.stopPropagation();
 } 
 
+menu.addEventListener("click", changeMenuToTimes)
 menu.addEventListener("click", openNav);
+//close.addEventListener("click", changeTimesToMenu);
 
 document.addEventListener("click", function(event) {
     if (!nav.contains(event.target)) {
@@ -24,3 +27,12 @@ document.addEventListener("click", function(event) {
       menu.setAttribute("src", "/starter-code/assets/shared/mobile/icon-hamburger.svg");
     }
 });
+
+
+
+// if (menu.getAttribute('src') === "/starter-code/assets/shared/mobile/icon-hamburger.svg") {
+//     menu.setAttribute('src', "/starter-code/assets/shared/mobile/icon-close.svg");
+// }
+//   else {
+//     menu.setAttribute('src', "/starter-code/assets/shared/mobile/icon-hamburger.svg");
+// }
